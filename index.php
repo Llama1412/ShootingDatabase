2bbe9b15e3bce18a4930093afdec217f88217018
Llama1412 <31854230+Llama1412@users.noreply.github.com>
2018-07-22 15:41:35 +0100

    Added the title bar to it.

--- index.php
+++ index.php
@@ -4,10 +4,16 @@
 
 <div class="container">
 	<div class="topbar" style="position: relative">
-		<img class="topimage" src="images\barback.jpg">
+		<img class="topimage" src="images\barback.jpg" height="125">
 		<div class="logo">
 		<img src="images\OundleLogo.png" height="95" width="95">
 		</div>
+	</div>
+	<div class="titlebar">
+	<h1>Oundle School Shooting Database</h1>
+	<div class="pagetitle">
+	<h2>Homepage</h2>
+	</div>
 	</div>
 </div>
 
