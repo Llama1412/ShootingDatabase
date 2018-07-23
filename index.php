bd7cee9f34139aaf75c36776fd8f041bc81fed57
Llama1412 <31854230+Llama1412@users.noreply.github.com>
2018-07-23 16:08:20 +0100

    added navbar

--- index.php
+++ index.php
@@ -11,14 +11,18 @@
 	</div>
 	<div class="titlebar">
 	<h1>Oundle School Shooting Database</h1>
-	<div class="pagetitle">
-	<h2>Homepage</h2>
+		<div class="pagetitle">
+			<h2 class="pagename">Homepage</h2>
+		</div>
 	</div>
-	</div>
+		<div class="navbar">
+			<div class="i0"><a href="index.php"><h2>Homepage</h2></a></div>
+			<div class="i1"><a href="deadlines.php"><h2>Competition Deadlines</h2></a></div>
+			<div class="i2"><a href="team.php"><h2>The Team<h2></a></div>
+			<div class="i3"><a href="scores.php"><h2>Scores</h2></a></div>
+			<div class="i4"><a href="gallery.php"><h2>Gallery</h2></a></div>
+			<div class="i5"><a href="admin.php"><h2>Admin</h2></a></div>
+		</div>
+	
 </div>
-
-
-
-
-
 <html>
