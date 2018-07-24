102b8b918362778104651d42cafb4e1528019411
Llama1412 <31854230+Llama1412@users.noreply.github.com>
2018-07-24 15:00:15 +0100

    Navbar

--- index.php
+++ index.php
@@ -12,16 +12,15 @@
 	<div class="titlebar">
 	<h1>Oundle School Shooting Database</h1>
 		<div class="pagetitle">
-			<h2 class="pagename">Homepage</h2>
 		</div>
 	</div>
 		<div class="navbar">
-			<div class="i0"><a href="index.php"><h2>Homepage</h2></a></div>
-			<div class="i1"><a href="deadlines.php"><h2>Competition Deadlines</h2></a></div>
-			<div class="i2"><a href="team.php"><h2>The Team<h2></a></div>
-			<div class="i3"><a href="scores.php"><h2>Scores</h2></a></div>
-			<div class="i4"><a href="gallery.php"><h2>Gallery</h2></a></div>
-			<div class="i5"><a href="admin.php"><h2>Admin</h2></a></div>
+			<div class="i0 active" onclick="location.href='index.php';"><h2>Homepage</h2></div>
+			<div class="i1" onclick="location.href='deadlines.php';"><h2>Competition Deadlines</h2></div>
+			<div class="i2" onclick="location.href='team.php';"><h2>The Team<h2></div>
+			<div class="i3" onclick="location.href='scores.php';"><h2>Scores</h2></div>
+			<div class="i4" onclick="location.href='gallery.php';"><h2>Gallery</h2></div>
+			<div class="i5" onclick="location.href='admin.php';"><h2>Admin</h2></div>
 		</div>
 	
 </div>
