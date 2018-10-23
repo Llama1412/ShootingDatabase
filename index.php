0c50e1b23891774abcf75dcbb74d2032a80e0922
Llama1412 <31854230+Llama1412@users.noreply.github.com>
2018-10-23 14:57:07 +0100

    Added AJAX Search

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php
+    $DS = DIRECTORY_SEPARATOR;
+    file_exists(__DIR__ . $DS . 'core' . $DS . 'Handler.php') ? require_once __DIR__ . $DS . 'core' . $DS . 'Handler.php' : die('Handler.php not found');
+    file_exists(__DIR__ . $DS . 'core' . $DS . 'Config.php') ? require_once __DIR__ . $DS . 'core' . $DS . 'Config.php' : die('Config.php not found');
+    
+    use AjaxLiveSearch\core\Config;
+    use AjaxLiveSearch\core\Handler;
+    
+    if (session_id() == '') {
+        session_start();
+    }
+    
+        $handler = new Handler();
+        $handler->getJavascriptAntiBot();
+    ?>
 <!doctype html>
 <html lang="en">
     <head>
@@ -7,6 +22,9 @@
         <title>Shooting Database</title>
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/jumbotron.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/ajaxlivesearch.min.css">
+        <link rel="stylesheet" href="css/fontello.css">
+        <link rel="stylesheet" href="css/animation.css">
     </head>
     <body>
         <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
@@ -28,8 +46,7 @@
                     </li>
                 </ul>
                 <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    <input type="text" class='mySearch input-medium' id="ls_query" placeholder="Search">
                 </form>
             </div>
         </nav>
@@ -176,7 +193,33 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
             crossorigin="anonymous"></script>
         <script>window.jQuery || document.write('<script src="js/jquery-slim.min.js"><\/script>')</script>
-        <script src="jspopper.min.js"></script>
+        <script src="js/popper.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="js/jquery-1.11.1.min.js"></script>
+        <!-- Live Search Script -->
+        <script type="text/javascript" src="js/ajaxlivesearch.min.js"></script>
+        <script></script>
+        <script>
+            jQuery(document).ready(function(){
+                jQuery(".mySearch").ajaxlivesearch({
+                    loaded_at: <?php echo time(); ?>,
+                    token: <?php echo "'" . $handler->getToken() . "'"; ?>,
+                    max_input: <?php echo Config::getConfig('maxInputLength'); ?>,
+                    onResultClick: function(e, data) {
+                        // get the index 0 (first column) value
+                        var selectedOne = jQuery(data.selected).find('td').eq('0').text();
+                        
+                    },
+                    onResultEnter: function(e, data) {
+                        // do whatever you want
+                        // jQuery("#ls_query").trigger('ajaxlivesearch:search', {query: 'test'});
+                    },
+                    onAjaxComplete: function(e, data) {
+            
+                    }
+                });
+            })
+        </script>
     </body>
 </html>
