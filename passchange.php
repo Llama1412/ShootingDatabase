b0532d7a95d2b107df952685285539fb8865a934
Llama1412 <31854230+Llama1412@users.noreply.github.com>
2018-12-01 09:36:57 +0000

    repositioned a loop

--- passchange.php
+++ passchange.php
@@ -3,15 +3,16 @@ session_start();
 include 'connection.php';
 
     $result = mysqli_query($connection,"SELECT * FROM credentials WHERE identifier = '1'");
+    while($row = mysqli_fetch_array($result))
+    {
+        $oldpass = $row['PassHash'];
+    };
+
     $currpass = $_POST["currpass"];
     $newpass = $_POST["newpass"];
     $currpasshash = hash("sha512", $currpass);
     $newpasshash = hash("sha512", $newpass);
     
-    while($row = mysqli_fetch_array($result))
-    {
-        $oldpass = $row['PassHash'];
-    };
 
     if ($currpasshash == $oldpass) {
         $query = "UPDATE credentials SET PassHash = '".$newpasshash."' WHERE identifier = '1'";
@@ -19,7 +20,7 @@ include 'connection.php';
         $_SESSION["message"] = "Password successfully changed!";
         header("location:login.php");    
     } else {
-        $_SESSION["message"] = "Did not work.";
+        $_SESSION["message"] = "Incorrect password!";
         header("location:login.php");
     }
 
