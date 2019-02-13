e5f77af1a4c8e01688eef14495855f4d3715ab8a
Llama1412 <31854230+Llama1412@users.noreply.github.com>
2019-02-13 13:50:15 +0000

    adjusted the connection php for localhost

--- connection.php
+++ connection.php
@@ -1,3 +1,4 @@
 <?php
-$connection = mysqli_connect("35.205.220.6", "root", "pass", "shootingdatabase");
+//$connection = mysqli_connect("35.205.220.6", "root", "pass", "shootingdatabase");
+$connection = mysqli_connect("localhost", "root", "", "shootingdatabase");
 ?>
