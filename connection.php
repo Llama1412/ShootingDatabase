ee23ff172c848755e443ede0669cef7f5f1ae2c4
Llama1412 <31854230+Llama1412@users.noreply.github.com>
2019-03-23 12:15:57 +0000

    changed the connection

--- connection.php
+++ connection.php
@@ -1,3 +1,3 @@
 <?php
-$connection = mysqli_connect("localhost", "root", "", "shootingdatabase");
+$connection = mysqli_connect("localhost", "eadbfcdd_sdb", "password123!", "eadbfcdd_shootingdatabase");
 ?>
